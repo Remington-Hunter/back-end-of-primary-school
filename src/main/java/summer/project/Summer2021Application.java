f3c5c9524c9fb79a116b73aefbfeca073d47a7aa
thweetkomputer <2320092610@qq.com>
2021-08-18 21:38:30 +0800

    初始化项目

---
+++ src/main/java/summer/project/Summer2021Application.java
@@ -0,0 +1,13 @@
+package summer.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summer2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Summer2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/summer/project/Summer2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package summer.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summer2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
