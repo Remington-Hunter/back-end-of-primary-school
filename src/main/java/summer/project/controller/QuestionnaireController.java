a4e69734b7701db70afa71bf3dcdd186d81efbce
thweetkomputer <2320092610@qq.com>
2021-08-22 10:59:09 +0800

    修改复制问卷外键错误的bug

--- src/main/java/summer/project/controller/QuestionnaireController.java
+++ src/main/java/summer/project/controller/QuestionnaireController.java
@@ -327,11 +327,15 @@ public class QuestionnaireController {
             questionnaireService.save(newQuestionnaire);
 
             List<Question> questionList = questionService.list(new QueryWrapper<Question>().eq("questionnaire", questionnaire.getId()));
+            for (Question question : questionList) {
+                question.setQuestionnaire(newQuestionnaire.getId());
+            }
             questionService.saveBatch(questionList);
 
             for (Question question : questionList) {
                 List<Option> optionList = optionService.list(new QueryWrapper<Option>().eq("question_id", question.getId()));
                 for (Option option : optionList) {
+                    option.setQuestionId(question.getId());
                     option.setAnswerNum(0L);
                 }
                 optionService.saveBatch(optionList);
