d90baca7a0eea0b8e6e809176834ccb9127d5e3c
thweetkomputer <2320092610@qq.com>
2021-08-23 16:28:36 +0800

    个人信息设置（未绑定）

--- src/main/java/summer/project/controller/UserController.java
+++ src/main/java/summer/project/controller/UserController.java
@@ -1,9 +1,22 @@
 package summer.project.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import summer.project.common.dto.EmailDto;
+import summer.project.common.lang.Result;
+import summer.project.entity.User;
+import summer.project.service.UserService;
+import summer.project.util.ShiroUtil;
 
 /**
  * <p>
@@ -13,8 +26,45 @@ import org.springframework.web.bind.annotation.RestController;
  * @author JerryZhao
  * @since 2021-08-20
  */
+@Api(tags = {"个人信息相关"})
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    @RequiresAuthentication
+    @PostMapping("/get_info")
+    @ApiOperation(value = "获取个人信息")
+    private Result getInfo() {
+        return Result.succeed(userService.getById(ShiroUtil.getProfile().getId()));
+    }
+
+    @RequiresAuthentication
+    @PostMapping("/set_phone")
+    @ApiOperation(value = "设置")
+    private Result setPhone(@ApiParam(value = "手机号（字符串）") String phone) {
+        User user = userService.getById(ShiroUtil.getProfile().getId());
+        user.setPhone(phone);
+        return Result.succeed(200, "手机号设置成功", user);
+    }
+
+    @RequiresAuthentication
+    @PostMapping("/set_email")
+    @ApiOperation(value = "设置")
+    private Result setEmail(@ApiParam(value = "邮箱（字符串）") @Validated EmailDto emailDto) {
+        User user = userService.getById(ShiroUtil.getProfile().getId());
+        user.setEmail(emailDto.getEmail());
+        return Result.succeed(200, "邮箱设置成功", user);
+    }
+
+    @RequiresAuthentication
+    @PostMapping("/set_wechat")
+    @ApiOperation(value = "设置")
+    private Result setWechat(@ApiParam(value = "微信号（字符串）") String wechat) {
+        User user = userService.getById(ShiroUtil.getProfile().getId());
+        user.setWechatId(wechat);
+        return Result.succeed(200, "微信设置成功", user);
+    }
 }
