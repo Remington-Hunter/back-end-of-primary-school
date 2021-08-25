8f2d8e1614a1aa4f3e790ec5f74818d85478f76d
thweetkomputer <2320092610@qq.com>
2021-08-25 11:36:09 +0800

    bug

--- src/test/java/summer/project/SendMessageTest.java
+++ src/test/java/summer/project/SendMessageTest.java
@@ -4,6 +4,7 @@ import com.github.qcloudsms.SmsSingleSender;
 import com.github.qcloudsms.SmsSingleSenderResult;
 import org.json.JSONException;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 import summer.project.util.VerifyCode;
 import summer.project.util.message.SMSParameter;
 
@@ -12,11 +13,33 @@ import java.io.IOException;
 
 public class SendMessageTest {
 
-    private final SMSParameter smsParameter = new SMSParameter();
+    @Value("${tencentcloud.sms.appId}")
+    public int appId;
+    // 短信应用SDK AppKey
+    @Value("${tencentcloud.sms.appKey}")
+    public String appKey;
+    // 需要发送短信的手机号码，可以定义多个手机号码
+    public String[] phoneNumbers = {"15166680628","15295791959"};
+    // 短信模板ID，需要在短信控制台中申请，我们查看自己的短信模板ID即可
+    @Value("${tencentcloud.sms.templateId}")
+    public int templateId;
+    // 签名，签名参数使用的是`签名内容`，而不是`签名ID`，真实的签名需要在短信控制台申请，这里按自己的来修改就好
+//    @Value("${tencentcloud.sms.smsSign}")
+    public String smsSign = "赵宸个人学生作业";
+
+    SMSParameter smsParameter = new SMSParameter();
+
+    @Value("${storage.pathname}")
+    private String a;
+
+    @Test
+    public void p() {
+        System.out.println(a);
+    }
 
     @Test
     public void testM() {
-        SMSParameter msParameter = new SMSParameter();
+        SMSParameter msParameter = new SMSParameter(appId, appKey, phoneNumbers, templateId, smsSign);
     }
 
     @Test
@@ -24,7 +47,7 @@ public class SendMessageTest {
         System.out.println(smsParameter);
         try {
             //短信模板中的参数列表
-            String[] params = {VerifyCode.createRandom(true, 4), "5"};
+            String[] params = {VerifyCode.createRandom(true, 4)};
             SmsSingleSender sender = new SmsSingleSender(smsParameter.getAppId(), smsParameter.getAppKey());
             SmsSingleSenderResult result = sender.sendWithParam("86", smsParameter.getPhoneNumbers()[0],
                     smsParameter.getTemplateId(), params, smsParameter.getSmsSign(), "", "");
